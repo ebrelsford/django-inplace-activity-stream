63c75d6dd3e0a518ecd474496f1ccc4611a6a07b
Eric Brelsford <ebrelsford@gmail.com>
2014-09-16 16:46:26 -0400

    Wrap action handler rather than reimplement

--- inplace_activity_stream/models.py
+++ inplace_activity_stream/models.py
@@ -1,11 +1,8 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.gis.db.models import PointField
-from django.contrib.contenttypes.models import ContentType
 
-from actstream import settings as actstream_settings
-from actstream.actions import now
-from actstream.exceptions import check_actionable_model
+from actstream.actions import action_handler
 from actstream.models import Action
 
 from .signals import action
@@ -13,36 +10,13 @@ from .signals import action
 
 def place_action_handler(verb, **kwargs):
     """
-    Handler function to create Action instance upon action signal call.
+    Add default actor if none exists, else use the action handler provided by
+    django-activity-stream.
     """
-    kwargs.pop('signal', None)
-
-    actor = kwargs.pop('sender', None)
+    actor = kwargs.get('sender', None)
     if not actor:
-        actor = User.objects.get(pk=settings.ACTIVITY_STREAM_DEFAULT_ACTOR_PK)
-    check_actionable_model(actor)
-
-    newaction = Action(
-        actor_content_type=ContentType.objects.get_for_model(actor),
-        actor_object_id=actor.pk,
-        verb=unicode(verb),
-        place=kwargs.pop('place', None),
-        public=bool(kwargs.pop('public', True)),
-        description=kwargs.pop('description', None),
-        timestamp=kwargs.pop('timestamp', now())
-    )
-
-    for opt in ('target', 'action_object'):
-        obj = kwargs.pop(opt, None)
-        if not obj is None:
-            check_actionable_model(obj)
-            setattr(newaction, '%s_object_id' % opt, obj.pk)
-            setattr(newaction, '%s_content_type' % opt,
-                    ContentType.objects.get_for_model(obj))
-
-    if actstream_settings.USE_JSONFIELD and len(kwargs):
-        newaction.data = kwargs
-    newaction.save()
+        kwargs['actor'] = User.objects.get(pk=settings.ACTIVITY_STREAM_DEFAULT_ACTOR_PK)
+    return action_handler(verb, **kwargs)
 
 
 # Use our action handler instead
